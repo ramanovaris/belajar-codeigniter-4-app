6ccaa34fa7ab8b50a1466ae20abcb017dc5af17c
Rama Novaris Ayyubi Pratama <ramaanovariss@gmail.com>
2022-11-30 12:11:25 +0800

    5. Views

---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h1>About Me</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure repellendus sunt officiis, excepturi iusto sit dolore inventore perspiciatis. Fugiat magni quia animi ex blanditiis doloribus neque dolore tenetur esse tempora.</p>
+    </div>
+  </div>
+</div>
